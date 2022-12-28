761d1e4a3ca64994db16ce7408528d8c5eb24619
saddamkhan2036 <saddamkhan.2036@gmail.com>
2022-12-28 23:07:19 +0400

    our store component - sidebar section

--- src/pages/OurStore.js
+++ src/pages/OurStore.js
@@ -1,12 +1,225 @@
 import React from "react";
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
+import ReactStars from "react-rating-stars-component";
 
 const OurStore = () => {
   return (
     <>
-     <Meta title={"Our Store"} />
+      <Meta title={"Our Store"} />
       <BreadCrumb title="Our Store" />
+      <div className="store-wrapper home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-3">
+              <div className="filter-card">
+                <h3 className="filter-title">Shop By Categories</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li>Watch</li>
+                    <li>Tv</li>
+                    <li>Camera</li>
+                    <li>Laptop</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card">
+                <h3 className="filter-title">Filter By</h3>
+                <div>
+                  <h5 className="sub-title">Availability</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        In Stock (1)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id=""
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        Out of Stock(0)
+                      </label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Price</h5>
+                  <div className="d-flex align-items-center gap-10 mb-3">
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control py-1"
+                        id="floatingInput"
+                        placeholder="Form"
+                      />
+                      <label htmlFor="floatingInput">Form</label>
+                    </div>
+                    <div className="form-floating">
+                      <input
+                        type="email"
+                        className="form-control py-1"
+                        id="floatingInput"
+                        placeholder="Form"
+                      />
+                      <label htmlFor="floatingInput">To</label>
+                    </div>
+                  </div>
+                  <h5 className="sub-title">Colors</h5>
+                  <div>
+                    <ul className="colors ps-0">
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                      <li></li>
+                    </ul>
+                  </div>
+                  <h5 className="sub-title">Size</h5>
+                  <div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-1"
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        S (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-2"
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        M (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-2"
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        L (2)
+                      </label>
+                    </div>
+                    <div className="form-check">
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value=""
+                        id="color-2"
+                      />
+                      <label className="form-check-label" htmlFor="">
+                        XL (2)
+                      </label>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Product Tags</h3>
+                <div>
+                  <div className="product-tags d-flex flex-wrap align-items-center gap-10 ">
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Headphone
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Laptop
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      Mobile
+                    </span>
+                    <span className="badge bg-light text-secondary rounded-3 py-2 px-3">
+                      labs
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3 className="filter-title">Random Product</h3>
+                <div>
+                  <div className="random-products mb-3 d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/watch.jpg"
+                        className="img-fluid"
+                        alt="watch"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Best iphone watches with good offer</h5>
+                      <reactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 300</b>
+                    </div>
+                  </div>
+                  <div className="random-products d-flex">
+                    <div className="w-50">
+                      <img
+                        src="images/headphone.jpg"
+                        className="img-fluid"
+                        alt="headphone"
+                      />
+                    </div>
+                    <div className="w-50">
+                      <h5>Best headphone with good offer</h5>
+                      <reactStars
+                        count={5}
+                        size={24}
+                        value={4}
+                        edit={false}
+                        activeColor="#ffd700"
+                      />
+                      <b>$ 200</b>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-9">
+              <div className="filter-sort-grid">
+                
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
