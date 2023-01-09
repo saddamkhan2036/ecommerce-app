f4bda52946b3db15d705f88346c9b7da2359edaa
saddamkhan2036 <saddamkhan.2036@gmail.com>
2023-01-09 17:19:39 +0400

    blog section

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function BlogCard() {
   return (
-    <div className='col-3'>
-        <div className="blog-card">
-            <div className="card-image">
-                <img src='images/blog-1.jpg' className='img-fluid' alt='blog' />
-            </div>
-            <div className="blog-content">
-                <p className='date'>15 Dec 2022</p>
-                <h5 className='title'>A beautiful thursday morning renaissance</h5>
-                <p className='desc'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis voluptatum nostrum perspiciatis sapiente laboriosam. Quis odit quod saepe sequi ipsa commodi dolores quas, laudantium laborum eveniet facere et ducimus eligendi!</p>
-                <Link to="/" className="button">Read More</Link>
-            </div>
+      <div className="blog-card">
+        <div className="card-image">
+          <img src="images/blog-1.jpg" className="img-fluid w-100" alt="blog" />
         </div>
-    </div>
-  )
+        <div className="blog-content">
+          <p className="date">15 Dec 2022</p>
+          <h5 className="title">A beautiful thursday morning renaissance</h5>
+          <p className="desc">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facilis
+            voluptatum nostrum perspiciatis sapiente laboriosam. Quis odit quod
+          </p>
+          <Link to="/" className="button">
+            Read More
+          </Link>
+        </div>
+      </div>
+  );
 }
